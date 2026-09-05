1d60368f1ee5adb9430e9be9498bcfe6c1eed48b
Mhn23 <mihneaserban@windowslive.com>
2026-09-06 00:13:08 +0300

    v2 fully functional and the way I want it

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ let sum = firstCard + secondCard
 let statusText = document.querySelector("#status-el")
 let cardsText = document.querySelector("#listOfCards")
 let sumText = document.querySelector("#sumOfCards")
+let hasLost = false
 
 function getRandomNumber(){
     return Math.floor(Math.random() * (10-1) + 1) + 1
 }
 
-function renderGame(){
+function cardsAndSumContent(){
     cardsText.textContent = cards
     sumText.textContent = sum
+}
+
+function renderGame(){
+    cardsAndSumContent()
     if(sum === 21){
         statusText.textContent = "You won!"
     }
@@ -20,15 +25,40 @@ function renderGame(){
         statusText.textContent = "Do you want to draw another card?"
     }else{
         statusText.textContent = "You lost!"
+        hasLost = true
     }
 }
+
+function youLostMessage(){
+    statusText.textContent = "You can't draw another card, reset the game !"
+}
+
 function addCard(theNewCard){
-    cards+=theNewCard
+    if(hasLost){
+        youLostMessage()
+    }
+    else{
+        cards.push(theNewCard)
+    }
 }
 
 function newCard(){
+    if(hasLost){
+        youLostMessage()
+    }
+    else{
     let theNewCard = getRandomNumber()
     addCard(theNewCard)
     sum += theNewCard
+    renderGame()
+    }
+}
+
+function resetGame(){
+    hasLost = false
+    firstCard = getRandomNumber()
+    secondCard = getRandomNumber()
+    cards = [firstCard,secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
