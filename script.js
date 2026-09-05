168d6b27c0c6f28226efb5e08335bb71102f7e49
Mhn23 <mihneaserban@windowslive.com>
2026-09-05 23:40:41 +0300

    v1 working

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-
-
+let firstCard = getRandomNumber()
+let secondCard = getRandomNumber()
+let cards = [firstCard,secondCard]
+let sum = firstCard + secondCard
+let statusText = document.querySelector("#status-el")
+let cardsText = document.querySelector("#listOfCards")
+let sumText = document.querySelector("#sumOfCards")
 
 function getRandomNumber(){
-    return Math.floor(Math.random() * (max-min) + min) + 1
+    return Math.floor(Math.random() * (10-1) + 1) + 1
+}
+
+function renderGame(){
+    cardsText.textContent = cards
+    sumText.textContent = sum
+    if(sum === 21){
+        statusText.textContent = "You won!"
+    }
+    else if(sum < 21){
+        statusText.textContent = "Do you want to draw another card?"
+    }else{
+        statusText.textContent = "You lost!"
+    }
+}
+function addCard(theNewCard){
+    cards+=theNewCard
+}
+
+function newCard(){
+    let theNewCard = getRandomNumber()
+    addCard(theNewCard)
+    sum += theNewCard
+    renderGame()
 }
